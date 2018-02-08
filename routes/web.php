1d65ac6eb807a763613d7fa8c5e61d2fa3ea60b6
Gavin <tianyu_0723@hotmail.com>
2018-02-08 14:27:54 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,7 @@ Route::group(['middleware' => ['web', 'wechat.oauth:default,snsapi_userinfo']], 
 // github 钩子
 Route::any('/github_hook', function (){
     echo shell_exec('cd /data/wwwroot/gh-hb');
-    echo shell_exec('sudo /usr/bin/git pull 2>&1');
+    echo shell_exec('/usr/bin/git pull 2>&1');
 });
 
 // ajax 请求...
