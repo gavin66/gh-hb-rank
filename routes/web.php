26e59d804f952b5c6140acc089ff0916794f3404
Gavin <tianyu_0723@hotmail.com>
2018-02-11 17:40:16 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::group(['middleware' => ['web', 'wechat.oauth:default,snsapi_userinfo']], 
 
 // github 钩子
 Route::any('/github_hook', function (){
-    echo shell_exec('cd /btdata/site/guohang-hongbao');
+    echo shell_exec('cd /data/wwwroot/hb2.hnqxs.com');
     echo shell_exec('/usr/bin/git pull 2>&1');
 });
 
